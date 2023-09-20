7a98e00440a858535bd0ad6ce53e579c81078e9f
DemetrS <tanastr5387@gmail.com>
2023-09-20 21:15:54 +0600

    задания: 0-3 готовы

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -7,3 +7,18 @@ print(string_literal)
 print(integer_literal)
 print(float_literal)
 print(boolean_literal)
+
+
+
+sting_literal = ("какой-то текст")
+print(string_literal)
+
+integer_literal = ("8")
+print(integer_literal)
+
+float_literal = ("5.567877")
+print(float_literal)
+
+boolean_literal = ("true")
+print(boolean_literal)
+
