0d58f889aa4cc5be92fb512c74e379fd5cf5e1a7
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-09-20 17:26:05 +0100

    Проверка ДЗ | 1. Начало работы
    - Правильно: №0,
    - Неправильно: №1, №3,
    - Улучшены: №0, №1, №2, №3,
    - На доработку:

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -1,24 +1,14 @@
-string_literal = ...  # здесь Ваш код
-integer_literal = ...  # здесь Ваш код
-float_literal = ...  # здесь Ваш код
-boolean_literal = ...  # здесь Ваш код
+string_literal = "какой-то текст"
+integer_literal = 8
+float_literal = 5.567877
+boolean_literal = True
 
 print(string_literal)
 print(integer_literal)
 print(float_literal)
 print(boolean_literal)
 
-
-
-sting_literal = ("какой-то текст")
-print(string_literal)
-
-integer_literal = ("8")
-print(integer_literal)
-
-float_literal = ("5.567877")
-print(float_literal)
-
-boolean_literal = ("true")
-print(boolean_literal)
-
+# print("какой-то текст")
+# print(8)
+# print(5.567877)
+# print(True)
